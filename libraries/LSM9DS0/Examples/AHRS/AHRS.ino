d1c693633978fec4f6c307318e2b3ef814dacf60
Philipp M. Scholl <scholl@ess.tu-darmstadt.de>
2014-05-23 23:28:53 +0200

    update LSM9DS0 sample

--- libraries/LSM9DS0/Examples/AHRS/AHRS.ino
+++ libraries/LSM9DS0/Examples/AHRS/AHRS.ino
@@ -8,25 +8,23 @@ boolean connection=false;
 unsigned long time=0;
 
 void setup() {
-  Wire.begin();
+  Serial.begin(115200);
+  Wire.begin(); // neccesary! otherwise no sensor will work
   delay(1000);
-  //Serial.print("testing connection: ");
-  
+
   sen.initialize();
   connection = sen.testConnection();
-  //Serial.println(  connection  ? "OK" : "FAILED");
-  
+  Serial.println(  connection  ? "OK" : "FAILED");
+
   sen.setGyroFullScale(2000);
   sen.setGyroOutputDataRate(LSM9DS0_RATE_190);
   sen.setGyroBandwidthCutOffMode(LSM9DS0_BW_HIGH);
   sen.setGyroDataFilter(LSM9DS0_LOW_PASS);
-  
+
   sen.setAccRate(LSM9DS0_ACC_RATE_200);
   sen.setAccFullScale(LSM9DS0_ACC_8G);
   sen.setAccAntiAliasFilterBandwidth(LSM9DS0_ACC_FILTER_BW_50);
-//  sen.setAccHighPassFilterMode(LSM9DS0_HPM_NORMAL);
-//  sen.setFilterEnable(true);
-  
+
   sen.setMagFullScale(LSM9DS0_MAG_4_GAUSS);
   sen.setMagOutputRate(LSM9DS0_M_ODR_100);
 }
@@ -36,7 +34,7 @@ void setup() {
 void loop() {
   if (!connection)
     return;
-   
+
    time = micros();
    measurement_t m = sen.getMeasurement();
    orientation_t *o = 
@@ -45,8 +43,8 @@ void loop() {
                   m.mx, m.my, m.mz,
                   (micros() - time)/1e6);
 
-   Serial.print(o->q0); Serial.print("\t");
-   Serial.print(o->q1); Serial.print("\t");
-   Serial.print(o->q2); Serial.print("\t");
-   Serial.print(o->q3); Serial.print("\n");
+   p(o->q0); p("\t");
+   p(o->q1); p("\t");
+   p(o->q2); p("\t");
+   p(o->q3); p("\n");
 }
